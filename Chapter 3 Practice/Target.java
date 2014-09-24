2a239bf1c21b38b49eb35115598f3053671fcfa1
ikbiel <ikbiel13@gmail.com>
2014-09-24 15:18:08 -0500

    Finished Target Lab, started chapter 4 notes

--- Chapter 3 Practice/Target.java
+++ Chapter 3 Practice/Target.java
@@ -32,23 +32,28 @@ public class Target
         Ellipse2D.Double base2 = new Ellipse2D.Double
             (xValue + 20, yValue + 20, targetXDiam - 40, targetYDiam - 40);
         Ellipse2D.Double base3 = new Ellipse2D.Double
-            (xValue + 20, yValue + 20, targetXDiam - 40, targetYDiam - 40);
+            (xValue + 40, yValue + 40, targetXDiam - 80, targetYDiam - 80);
         Ellipse2D.Double base4 = new Ellipse2D.Double
-            (xValue + 20, yValue + 20, targetXDiam - 40, targetYDiam - 40);
+            (xValue + 60, yValue + 60, targetXDiam - 120, targetYDiam - 120);
         Ellipse2D.Double base5 = new Ellipse2D.Double
-            (xValue + 20, yValue + 20, targetXDiam - 40, targetYDiam - 40);
+            (xValue + 80, yValue + 80, targetXDiam - 160, targetYDiam - 160);
         
         
         g2.setColor(Color.BLACK);
         g2.draw(targetBase);
+        g2.fill(targetBase);
         g2.setColor(Color.WHITE);
         g2.draw(base2);
+        g2.fill(base2);
         g2.setColor(Color.BLACK);
         g2.draw(base3);
+        g2.fill(base3);
         g2.setColor(Color.WHITE);
         g2.draw(base4);
+        g2.fill(base4);
         g2.setColor(Color.BLACK);
         g2.draw(base5);
+        g2.fill(base5);
         
      
     }
